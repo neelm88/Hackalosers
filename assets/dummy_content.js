f5c49d7abf54e015ae662e959ffa072b20b88294
tanner210 <tanner.210@osu.edu>
2019-11-03 10:12:28 -0500

    content

---
+++ assets/dummy_content.js
@@ -0,0 +1,53 @@
+//Dummy content to show
+//You can also use dynamic data by calling webservice
+const CONTENT = [
+    {
+      isExpanded: false,
+      clubName: '1 Day For The K.I.A., Inc.',
+      missionCategory: [{ id: 'Mission' , val: '1 Day for the K.I.A.\u2019s purpose is to raise awareness and commemorate fallen military service members. 1 Day for the K.I.A. accomplishes our ...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=cef99280-7d97-4496-9396-900def432d97&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: '1girl',
+      missionCategory: [{ id: 'Mission' , val: 'To provide girls everywhere the opportunity to develop the skills and confidence they need to lead fulfilling lives and become successful le...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=9c37c72e-3852-475c-8605-32f8594b5dbc&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: '3D Urban Dance Team',
+      missionCategory: [{ id: 'Mission' , val: 'Our organization will provide a creative outlet for aspiring dancers. Our organization also strives to expose our members as well as our aud...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=b906b1c1-e37b-402c-91b5-20787e5ae5c7&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: 'Alpha Chi Omega',
+      missionCategory: [{ id: 'Mission' , val: 'The purpose of Alpha Chi Omega Fraternity is \u201cto encourage the spirit of true sisterhood, to develop through personal effort a high moral an...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=d7f8f85f-8b5e-4795-bd51-e48fafac8992&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: 'Buckeyes for WHEP',
+      missionCategory: [{ id: 'Mission' , val: 'WHEP at Ohio State aims to support WHEP (Womens Health Education Program) through fundraising, donation drives, and mentorship for moms. In...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=f50a23aa-8ca9-42f0-b82d-b50c2448a638&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: 'Buckeyes in the Making',
+      missionCategory: [{ id: 'Mission' , val: 'To create/restrengthen the bond between OSU and the Columbus community. This will be done by providing the underprivileged children of Colum...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=f6569dde-fb1e-40d5-b1ab-73e3cbfa2dea&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: 'Information Systems Association',
+      missionCategory: [{ id: 'Mission' , val: 'ISA is an organization dedicated to providing MIS, CIS, and CSE students with opportunities to network with technology professionals, profes...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=48c0269f-86ff-4845-b809-9018eb58266c&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+    {
+      isExpanded: false,
+      clubName: 'Institute of Industrial & Systems Engineers',
+      missionCategory: [{ id: 'Mission' , val: 'To be the premier Industrial Engineering student organization committed to developing professional, academic, and social networks that openl...' }],
+      infoCategory: [{ id: 'More info' , val: 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=e22b0410-2439-4a45-83f2-b4aac40a2719&l=ALL&v=list&c=Columbus&page=0' }],
+    },
+  
+  ];
