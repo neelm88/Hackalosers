b4d5cddd4a8d967c70eda0193e2ebf136ee8e246
neelm88 <neelm88@gmail.com>
2019-11-03 01:04:37 -0500

    models created

--- tutorial/manage.py
+++ tutorial/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    print("x")
 
 
 if __name__ == '__main__':
---
+++ tutorial/snippets/views.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.models import User 
+from rest_framework import generics, permissions, status
+from .permissions import IsOwnerOrReadOnly
+from .models import Snippet, UserData
+from .serializers import SnippetSerializer, UserSerializer, UserDataSerializer
+from rest_framework.decorators import api_view # new
+from rest_framework.response import Response # new
+from rest_framework.reverse import reverse # new
+from rest_framework import generics, permissions, renderers # new
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,) # new
+
+    def perform_create(self, serializer): # new
+        serializer.save(owner=self.request.user)
+
+class SnippetHighlight(generics.GenericAPIView): # new
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,) # new
+
+
+
+class UserList(generics.ListAPIView): # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,) # new
+
+
+
+class UserDetail(generics.RetrieveAPIView): # new
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,) # new
+
+class UserDataList(generics.ListCreateAPIView):
+    queryset = UserData.objects.all()
+    serializer_class = UserDataSerializer
+    # def perform_create(self, serializer): # new
+    #     serializer.save(owner=self.request.user)
+
+
+class UserDataDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserData.objects.all()
+    serializer_class = UserDataSerializer
+
+@api_view(['GET']) # new
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'user_data': reverse('user-data-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+@api_view(['GET','POST'])
+def user_data_list(request, format=None):
+    if request.method == 'GET':
+        data = UserData.objects.all()
+        serializer = UserDataSerializer(data, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = UserDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
