71bc5a6e9e8c85f59d7e649a8e8db6c5d3159da8
tanner210 <tanner.210@osu.edu>
2019-11-03 04:33:02 -0500

    stuffs

--- node_modules/react-native-multiple-select/index.js
+++
@@ -1,8 +0,0 @@
-/*!
- * react-native-multi-select
- * Copyright(c) 2017 Mustapha Babatunde Oluwaleke
- * MIT Licensed
- */
-import MultiSelect from './lib/react-native-multi-select';
-
-export default MultiSelect;
--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Button, Text,TitleText, View,TextInput, TouchableOpacity, Dimensions, Image} from 'react-native';
 import { AnimatedBackgroundColorView } from 'react-native-animated-background-color-view';
-import { LoadingScreen } from 'react-loading-screen';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -9,17 +8,8 @@ export default class Login extends React.Component {
         this.state = {
             username: '', 
             password: '',
-            loading: true
         };
-      }
-
-    componentDidMount () {
-        // fake promise
-        setTimeout(() =>
-          this.setState({ loading: false })
-        , 1500)
-    }
-    
+      }   
 
     storeUsername = (input) => {
         this.setState({username: input})
@@ -30,18 +20,8 @@ export default class Login extends React.Component {
     }
 
     render(){
-        const { loading } = this.state
         return (
-            <AnimatedBackgroundColorView color='#a9dbc0' initialColor='#f5b5f1' duration={60000} style = {style_login.container}>
-                <LoadingScreen
-                    loading={true}
-                    bgColor='#f1f1f1'
-                    spinnerColor='#9ee5f8'
-                    textColor='#676767'
-                    logoSrc='/logo.png'
-                    text='Here an introduction sentence (Optional)'
-                /> 
-
+            <AnimatedBackgroundColorView color='#a9dbc0' initialColor='#f5b5f1' duration={60000} style = {style_login.container}>  
                 <Image source={{uri : 'https://img1.picmix.com/output/stamp/normal/3/5/1/1/1321153_5c59f.gif'}} style={{position: 'absolute', top: 20, width: 400, height: 400}}/>
                 <Image source={require('../assets/hubbub.png')} style={{width: 400, height: 200}}/>
                 <Text style = {style_login.signIn}> Sign In</Text>
